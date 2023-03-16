ed9ccfef52b097f16cdee78943af128174db5329
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-16 15:45:02 -0300

    style: schedule steps pages interface

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,43 @@
+import { Calendar } from '@/components/Calendar'
+import {
+  CalendarStepContainer,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+  const isDateSelected = true
+
+  return (
+    <CalendarStepContainer isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira, <span>20 de abril</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem disabled>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </CalendarStepContainer>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,12 @@
+import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+
+export function ScheduleForm() {
+  return (
+    <>
+      {/* <CalendarStep /> */}
+
+      <ConfirmStep />
+    </>
+  )
+}
