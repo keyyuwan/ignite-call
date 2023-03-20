7c4208cea60afb7133a39b4213c29f17db898b89
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-20 20:31:25 -0300

    feat: create event on user google calendar

---
+++ src/lib/google.ts
@@ -0,0 +1,76 @@
+/* eslint-disable camelcase */
+import dayjs from 'dayjs'
+import { google } from 'googleapis'
+import { prisma } from './prisma'
+
+/**
+ * getGoogleOAuthToken:
+ * Vai pegar o token que precisa ser passado nas requisições pra API do Google
+ * 
+ * Como?
+   1. Verificar no bd se o token do usuário expirou
+   2. Se sim, pega um novo token enviando o refresh token
+ * 
+ */
+
+// OBS: Sempre vai ser chamada quando formos fazer uma chamada pra API do Google
+
+export async function getGoogleOAuthToken(userId: string) {
+  const account = await prisma.account.findFirstOrThrow({
+    where: {
+      provider: 'google',
+      user_id: userId,
+    },
+  })
+
+  const auth = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+  )
+
+  auth.setCredentials({
+    access_token: account.access_token,
+    refresh_token: account.refresh_token,
+    expiry_date: account.expires_at ? account.expires_at * 1000 : null,
+  })
+
+  if (!account.expires_at) {
+    return auth
+  }
+
+  const isTokenExpired = dayjs(account.expires_at * 1000).isBefore(new Date())
+
+  if (isTokenExpired) {
+    const { credentials } = await auth.refreshAccessToken()
+    const {
+      access_token,
+      refresh_token,
+      expiry_date,
+      id_token,
+      scope,
+      token_type,
+    } = credentials
+
+    await prisma.account.update({
+      where: {
+        id: account.id,
+      },
+      data: {
+        access_token,
+        refresh_token,
+        expires_at: expiry_date ? Math.floor(expiry_date / 1000) : null,
+        id_token,
+        scope,
+        token_type,
+      },
+    })
+
+    auth.setCredentials({
+      access_token,
+      refresh_token,
+      expiry_date,
+    })
+  }
+
+  return auth
+}
