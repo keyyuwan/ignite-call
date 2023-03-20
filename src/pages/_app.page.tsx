7c31e822f792bfcaa84377f42a7199ff39a39d0c
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-03-19 21:40:50 -0300

    chore: Add react query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,15 +2,19 @@ import '@/lib/dayjs'
 
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
 
+import { queryClient } from '@/lib/react-query'
 import { globalStyles } from '@/styles/global'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
